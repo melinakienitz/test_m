18aac7cd56e394b7cafb809d19c2750af6e9d42f
melinakienitz <124668167+melinakienitz@users.noreply.github.com>
2023-11-02 14:26:04 +0100

    Building help of my function

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,5 +1,17 @@
-# This function splits a string with defined delimiter and returns the result as a vector
 
+#' Split a String
+#'
+#' @param x A Character vector with one element
+#' @param split What to split on
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "a,b,c,d,e,f,g"
+#' strsplit1(x, split =",)
+#' @author Melina Kienitz
+#'
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
